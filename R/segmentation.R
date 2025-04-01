87831608169ed2f46714730c4d55394de734a491
Nicklas Nordborg <nicklas.nordborg@med.lu.se>
2025-04-01 08:57:41 +0200

    Use outputfile prefix for all files.

--- R/segmentation.R
+++ R/segmentation.R
@@ -175,7 +175,7 @@ segment.baf.phased.sv = function(samplename, inputfile, outputfile, svs=NULL, ga
 #' @param calc_seg_baf_option Various options to recalculate the BAF of a segment. Options are: 1 - median, 2 - mean, 3 - ifelse median==0 or 1, median, mean. (Default: 3)
 #' @author sd11
 #' @export
-segment.baf.phased = function(samplename, inputfile, outputfile, prior_breakpoints_file=NULL, gamma=10, phasegamma=3, kmin=3, phasekmin=3, no_segmentation=F, calc_seg_baf_option=3) {
+segment.baf.phased = function(samplename, inputfile, outputprefix, prior_breakpoints_file=NULL, gamma=10, phasegamma=3, kmin=3, phasekmin=3, no_segmentation=F, calc_seg_baf_option=3) {
   # Function that takes SNPs that belong to a single segment and looks for big holes between
   # each pair of SNPs. If there is a big hole it will add another breakpoint to the breakpoints data.frame
   addin_bigholes = function(breakpoints, positions, chrom, startpos, maxsnpdist) {
@@ -365,19 +365,20 @@ segment.baf.phased = function(samplename, inputfile, outputfile, prior_breakpoin
       BAFoutputchr = rbind(BAFoutputchr, BAFoutput_preseg)
     }
     
-    png(filename = paste(samplename,"_RAFseg_chr",chr,".png",sep=""), width = 2000, height = 1000, res = 200, type = "cairo")
+    imageTitle = paste0(samplename, ", ", chr)
+    png(filename = paste0(outputprefix, chr, "_RAFseg.png"), width = 2000, height = 1000, res = 200, type = "cairo")
     create.segmented.plot(chrom.position=BAFoutputchr$Position/1000000, 
                           points.red=BAFoutputchr$BAF, 
                           points.green=BAFoutputchr$tempBAFsegm, 
                           x.min=min(BAFoutputchr$Position)/1000000, 
                           x.max=max(BAFoutputchr$Position)/1000000, 
-                          title=paste(samplename,", chromosome ", chr, sep=""), 
+                          title=imageTitle, 
                           xlab="Position (Mb)", 
                           ylab="BAF (phased)",
                           prior_bkps_pos=bkps_chrom$position/1000000)
     dev.off()
     
-    png(filename = paste(samplename,"_segment_chr",chr,".png",sep=""), width = 2000, height = 1000, res = 200, type = "cairo")
+    png(filename = paste0(outputprefix, chr, "_segment.png"), width = 2000, height = 1000, res = 200, type = "cairo")
     create.baf.plot(chrom.position=BAFoutputchr$Position/1000000, 
                     points.red.blue=BAFoutputchr$BAF, 
                     plot.red=BAFoutputchr$tempBAFsegm>0.5,
@@ -385,7 +386,7 @@ segment.baf.phased = function(samplename, inputfile, outputfile, prior_breakpoin
                     points.darkblue=1-BAFoutputchr$BAFseg, 
                     x.min=min(BAFoutputchr$Position)/1000000, 
                     x.max=max(BAFoutputchr$Position)/1000000, 
-                    title=paste(samplename,", chromosome ", chr, sep=""), 
+                    title=imageTitle, 
                     xlab="Position (Mb)", 
                     ylab="BAF (phased)",
                     prior_bkps_pos=bkps_chrom$position/1000000)
@@ -396,7 +397,7 @@ segment.baf.phased = function(samplename, inputfile, outputfile, prior_breakpoin
     BAFoutput = rbind(BAFoutput, BAFoutputchr[,c(1:5)])
   }
   colnames(BAFoutput) = c("Chromosome","Position","BAF","BAFphased","BAFseg")
-  write.table(BAFoutput, outputfile, sep="\t", row.names=F, col.names=T, quote=F)
+  write.table(BAFoutput, paste0(outputprefix, "BAFsegmented.txt"), sep="\t", row.names=F, col.names=T, quote=F)
 }
 
 
